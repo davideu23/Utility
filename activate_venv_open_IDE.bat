c7446e28325d25f120454f8105bf75afe036e9f1
Davide <50266282+davideu23@users.noreply.github.com>
2022-02-13 21:37:56 +0100

    some fix

--- activate_venv_open_IDE.bat
+++ activate_venv_open_IDE.bat
@@ -1,13 +1,26 @@
+REM -- @echo off --> do not print command executed
 @echo off
+
+:venvName
 echo Input name of the new enviroment to activate it.
 set /p venv_name=Name of venv: 
-cd %venv_name%\Scripts\
-choice /c sin /n /m "Do you want to open (s)pyder, (i)dle or (n)one"
-goto %ERRORLEVEL%
-:1
-activate.bat && spyder.exe
-:2
-activate.bat && python -m idlelib.idle
+
+
+:checkInput
+if exist %venv_name%\Scripts\activate.bat (
+	cd %venv_name%\Scripts\
+	goto selectIDE
+) else (
+	echo Virtual enviroment not found.
+	goto venvName
+)
+:selectIDE
+choice /C SIN  /m "Do you want to open (s)pyder, (i)dle or (n)one"
+	goto %ERRORLEVEL%
 :3
 start activate.bat
 pause
+:2
+activate.bat && python -m idlelib.idle
+:1
+activate.bat && spyder.exe
