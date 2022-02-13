60576a36c5b8bf3246e4e95eeb5df15a8438963d
Davide <50266282+davideu23@users.noreply.github.com>
2022-02-13 21:52:00 +0100

    Add files via upload

--- create_venv.bat
+++ create_venv.bat
@@ -1,16 +1,22 @@
 @echo off
+:venvName
 echo Input name of the new enviroment to create it a new one.
 set /p venv_name=Name of venv: 
-
+:createVenv
+REM python -m venv %venv_name%
 python -m venv %venv_name%
-echo Successfuly created a new venv in %cd%\%venv_name%
-
+if %errorlevel% == 0 (
+	echo Successfuly created a new venv in %cd%\%venv_name%
+	goto activateVenv
+) else (
+	echo Error found please check your enviroment variable
+	pause
+	exit
+)
+:activateVenv
 choice /c yn /n /m "Do you want to activate your venv (y/n)"
-set INPUT=%ERRORLEVEL%
-if %INPUT% EQU 1 goto 1
-if %INPUT% EQU 2 goto 2
+GOTO %errorlevel%
 :1
-cd %venv_name%\Scripts\
-start activate.bat
+start %venv_name%\Scripts\activate.bat
 :2
-pause
+exit
